9e9e01e6e3605b48a903b7eb77574488ba050921
Kruben Naidoo <naidook@cput.ac.za>
2023-03-01 10:43:20 +0200

    Added a method to find the sum of two integers

---
+++ src/main/java/za/ac/cput/Calc.java
@@ -0,0 +1,13 @@
+package za.ac.cput;
+
+public class Calc {
+    /**
+     *
+     * @param a
+     * @param b
+     * @return int
+     */
+    public static int add(int a, int b) {
+        return (a + b);
+    }
+}
