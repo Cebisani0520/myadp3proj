46196cc56f9ef758f52ffaa1cfebb73594737b76
Kruben Naidoo <naidook@cput.ac.za>
2023-03-01 10:49:29 +0200

    Added a method to find the sum of two real numbers

--- src/main/java/za/ac/cput/Calc.java
+++ src/main/java/za/ac/cput/Calc.java
@@ -2,12 +2,22 @@ package za.ac.cput;
 
 public class Calc {
     /**
-     *
-     * @param a
-     * @param b
+     * adds two integers
+     * @param a int
+     * @param b int
      * @return int
      */
     public static int add(int a, int b) {
+        return (a + b);
+    }
+
+    /**
+     * adds two real numbers
+     * @param a double
+     * @param b double
+     * @return double
+     */
+    public static double add(double a, double b) {
         return (a + b);
     }
 }
